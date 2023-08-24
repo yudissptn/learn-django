95f241601103d5f7b765d78459c7e6451ad4f1d9
Yudhist <yudissptn@gmail.com>
2023-08-24 22:09:07 +0700

    done

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import AddRecordForm, SignUpForm
 from .models import Record
 
 
@@ -66,6 +66,35 @@ def delete_record(request, pk):
         delete_it.delete()
         messages.success(request, "Record deleted successfully")
         return redirect('home')
+    else:
+        messages.success(request, "You Must be logged in to view")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record added successfully")
+                return redirect('home')
+
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must be logged in to view")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated successfully")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
     else:
         messages.success(request, "You Must be logged in to view")
         return redirect('home')
